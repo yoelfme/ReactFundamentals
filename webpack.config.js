53c9396a3aeefbeb9556421de9a3ff37b26417ec
Yoel Monzon <yoelfme@hotmail.com>
2016-09-18 02:13:57 -0600

    [add] dependency of webpack and html webpack plugin configuration to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
 module.exports = {
   entry: [
     './app/index.js'
@@ -10,5 +17,6 @@ module.exports = {
   output: {
     filename: 'index_bundle.js',
     path: __dirname + '/dist'
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 }
