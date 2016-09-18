fa623daf5fcb52806f59994338eaa2122313e637
Yoel Monzon <yoelfme@hotmail.com>
2016-09-18 02:18:57 -0600

    [add] configuration for babel, and loader of babel

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   ],
   module: {
     loaders: [
-      { test: /\.coffee$/, exclude: /node_modules/, loader: 'coffee-loader' }
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
     ]
   },
   output: {
