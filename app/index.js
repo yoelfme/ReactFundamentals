e14e6fa7dcad38360dc3a1e64dea42b7b92fd136
Yoel Monzon <yoelfme@hotmail.com>
2016-09-18 18:53:46 -0600

    [add] new component and nested components with pure functions

--- app/index.js
+++ app/index.js
@@ -1,12 +1,50 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
+'use strict'
 
-var HelloWorld = React.createClass({
-  render: function () {
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const USER_DATA = {
+  name: 'Yoel Monzon',
+  username: 'yoelfme',
+  image: 'https://avatars3.githubusercontent.com/u/4493680?v=3&s=466'
+}
+
+const ProfilePic = React.createClass({
+  render () {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+})
+
+const ProfileLink = React.createClass({
+  render () {
     return (
-      <div>Hello Reactjs Program</div>
+      <div>
+        <a href={`https://www.github.com/${this.props.username}`} target='_blank'>
+          {this.props.username}
+        </a>
+      </div>
     )
   }
 })
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'))
+const ProfileName = React.createClass({
+  render () {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+})
+
+const Avatar = React.createClass({
+  render () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'))
