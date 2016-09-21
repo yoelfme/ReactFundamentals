efecc7a80773c91a9579d2952dafbfbfab07d8e6
Yoel Monzon <yoelfme@hotmail.com>
2016-09-21 04:06:02 -0600

    [udpate] convert prompt from presentational component to presentational functional component

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -3,39 +3,40 @@
 import React from 'react'
 import Styles from '../styles'
 
-const PropTypes = React.PropTypes
+const { string, func } = React.PropTypes
 
-export default React.createClass({
-  propTypes: {
-    header: PropTypes.string.isRequired,
-    username: PropTypes.string.isRequired,
-    onUpdateUser: PropTypes.func.isRequired,
-    onSubmitUser: PropTypes.func.isRequired
-  },
-  render () {
-    return (
-      <div className='jumbotron col-sm-6 col-sm-offset-3 text-center' style={Styles.transparentBg}>
-        <h1>{this.props.header}</h1>
-        <div className='col-sm-12'>
-          <form onSubmit={this.props.onSubmitUser}>
-            <div className='form-group'>
-              <input
-                className='form-control'
-                placeholder='Github username'
-                onChange={this.props.onUpdateUser}
-                value={this.props.username}
-                type='text'/>
-            </div>
-            <div className='form-group col-sm-4 col-sm-offset-4'>
-              <button
-                className='btn btn-block btn-success'
-                type='submit'>
-                  Continue
-              </button>
-            </div>
-          </form>
-        </div>
+const Prompt = (props) => {
+  return (
+    <div className='jumbotron col-sm-6 col-sm-offset-3 text-center' style={Styles.transparentBg}>
+      <h1>{props.header}</h1>
+      <div className='col-sm-12'>
+        <form onSubmit={props.onSubmitUser}>
+          <div className='form-group'>
+            <input
+              className='form-control'
+              placeholder='Github username'
+              onChange={props.onUpdateUser}
+              value={props.username}
+              type='text'/>
+          </div>
+          <div className='form-group col-sm-4 col-sm-offset-4'>
+            <button
+              className='btn btn-block btn-success'
+              type='submit'>
+                Continue
+            </button>
+          </div>
+        </form>
       </div>
-    )
-  }
-})
+    </div>
+  )
+}
+
+Prompt.propTypes = {
+  header: string.isRequired,
+  username: string.isRequired,
+  onUpdateUser: func.isRequired,
+  onSubmitUser: func.isRequired
+}
+
+export default Prompt
