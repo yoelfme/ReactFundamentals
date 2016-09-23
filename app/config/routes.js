4880da857b401215b24899447871925404930902
Yoel Monzon <yoelfme@hotmail.com>
2016-09-23 02:42:27 -0600

    [add] route for results component

--- app/config/routes.js
+++ app/config/routes.js
@@ -7,6 +7,7 @@ import Main from '../components/Main'
 import Home from '../components/Home'
 import PromptContainer from '../containers/PromptContainer'
 import ConfirmBattleContainer from '../containers/ConfirmBattleContainer'
+import ResultsContainer from '../containers/ResultsContainer'
 
 const routes = (
   <Router history={hashHistory}>
@@ -15,6 +16,7 @@ const routes = (
       <Route path='/playerOne' header='Player One' component={PromptContainer} />
       <Route path='/playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
       <Route path='/battle' component={ConfirmBattleContainer} />
+      <Route path='/results' component={ResultsContainer} />
     </Route>
   </Router>
 )
